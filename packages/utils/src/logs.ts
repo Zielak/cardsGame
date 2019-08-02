e83d48ab2f5d113da9da8cca7a39e71996c3046a
Darek Zieliński <dariusz.zielinski@dazn.com>
2019-08-02 22:19:35 +0200

    back to previous logs

--- packages/utils/src/logs.ts
+++ packages/utils/src/logs.ts
@@ -16,83 +16,7 @@ export enum LogLevels {
 
 let logLevel = LogLevels.silent
 
-const syntaxHighlight = (arg: any) => {
-  if (typeof arg === "string") {
-    return chalk.gray(arg)
-  }
-  if (typeof arg === "number") {
-    return chalk.red.bold("" + arg)
-  }
-  if (typeof arg === "boolean") {
-    return chalk.green.bold(arg.toString())
-  }
-  // It must be some Entity
-  if (arg && arg._state) {
-    return chalk.yellow(minifyEntity(arg))
-  }
-  return arg
-}
-
-const minifyEntity = ({ type, name }): string => {
-  return `${type}:${name}`
-}
-
-const getLogsAPI = isBrowser => {
-  return isBrowser
-    ? {
-        verbose: console.debug.bind(window.console),
-        notice: console.log.bind(window.console),
-        info: console.info.bind(window.console),
-        warn: console.warn.bind(window.console),
-        error: console.error.bind(window.console)
-      }
-    : {
-        verbose: function(...args: any[]) {
-          console.log.apply(console, [
-            `\t`,
-            ...args.map(arg => chalk.gray(arg))
-          ])
-        },
-        notice: function(first, ...args: any[]) {
-          if (args.length > 0) {
-            console.log.apply(console, [
-              `${first}:`,
-              ...args.map(syntaxHighlight)
-            ])
-          } else {
-            console.log.call(console, chalk.gray(`\t${first}`))
-          }
-        },
-        info: function(first, ...args: any[]) {
-          console.info.apply(console, [
-            chalk.bgBlue.black(` ${first} `),
-            ...args.map(syntaxHighlight)
-          ])
-        },
-        warn: function(first, ...args: any[]) {
-          console.warn.apply(console, [
-            chalk.bgYellow.black(` ${first} `),
-            ...args.map(syntaxHighlight)
-          ])
-        },
-        error: function(first, ...args: any[]) {
-          console.error.apply(console, [
-            chalk.bgRed(` ${first} `),
-            ...args.map(syntaxHighlight)
-          ])
-        }
-      }
-}
-
-export let logs: {
-  error: (...args: any[]) => void
-  warn: (...args: any[]) => void
-  info: (...args: any[]) => void
-  notice: (...args: any[]) => void
-  verbose: (...args: any[]) => void
-} = getLogsAPI(isBrowser)
-
-export const setLogLevel = (val: string) => {
+const setLogLevel = (val: string) => {
   switch (val) {
     case "silent":
       logLevel = LogLevels.silent
@@ -118,33 +42,100 @@ export const setLogLevel = (val: string) => {
     default:
       logLevel = LogLevels.silent
   }
-
-  logs = getLogsAPI(isBrowser)
-
-  // Override log functions in case of lower desired audacity
-  if (logLevel < LogLevels.error) {
-    logs.error = noop
-  }
-  if (logLevel < LogLevels.warn) {
-    logs.warn = noop
-  }
-  if (logLevel < LogLevels.info) {
-    logs.info = noop
-  }
-  if (logLevel < LogLevels.notice) {
-    logs.notice = noop
-  }
-  if (logLevel < LogLevels.verbose) {
-    logs.verbose = noop
-  }
 }
+
+const syntaxHighlight = (arg: any) => {
+  if (typeof arg === "string") {
+    return chalk.gray(arg)
+  }
+  if (typeof arg === "number") {
+    return chalk.red.bold("" + arg)
+  }
+  if (typeof arg === "boolean") {
+    return chalk.green.bold(arg.toString())
+  }
+  // It must be some Entity
+  if (arg && arg._state) {
+    return chalk.yellow(minifyEntity(arg))
+  }
+  return arg
+}
+
+const minifyEntity = ({ type, name }): string => {
+  return `${type}:${name}`
+}
+
+export let logs: {
+  error: (...args: any[]) => void
+  warn: (...args: any[]) => void
+  info: (...args: any[]) => void
+  notice: (...args: any[]) => void
+  verbose: (...args: any[]) => void
+} = isBrowser
+  ? {
+      verbose: console.debug.bind(window.console),
+      notice: console.log.bind(window.console),
+      info: console.info.bind(window.console),
+      warn: console.warn.bind(window.console),
+      error: console.error.bind(window.console)
+    }
+  : {
+      verbose: function(...args: any[]) {
+        console.log.apply(console, [`\t`, ...args.map(arg => chalk.gray(arg))])
+      },
+      notice: function(first, ...args: any[]) {
+        if (args.length > 0) {
+          console.log.apply(console, [
+            `${first}:`,
+            ...args.map(syntaxHighlight)
+          ])
+        } else {
+          console.log.call(console, chalk.gray(`\t${first}`))
+        }
+      },
+      info: function(first, ...args: any[]) {
+        console.info.apply(console, [
+          chalk.bgBlue.black(` ${first} `),
+          ...args.map(syntaxHighlight)
+        ])
+      },
+      warn: function(first, ...args: any[]) {
+        console.warn.apply(console, [
+          chalk.bgYellow.black(` ${first} `),
+          ...args.map(syntaxHighlight)
+        ])
+      },
+      error: function(first, ...args: any[]) {
+        console.error.apply(console, [
+          chalk.bgRed(` ${first} `),
+          ...args.map(syntaxHighlight)
+        ])
+      }
+    }
 
 try {
   if (isBrowser && localStorage && localStorage.getItem("cardsDebug")) {
     setLogLevel(localStorage.getItem("cardsDebug"))
   } else if (!isBrowser) {
-    setLogLevel(process.env.LOGS)
+    setLogLevel(process.env.IN_PASSENGER ? "verbose" : process.env.LOGS)
   }
 } catch (e) {
   // disabled
+}
+
+// Override log functions in case of lower desired audacity
+if (logLevel < LogLevels.error) {
+  logs.error = noop
+}
+if (logLevel < LogLevels.warn) {
+  logs.warn = noop
+}
+if (logLevel < LogLevels.info) {
+  logs.info = noop
+}
+if (logLevel < LogLevels.notice) {
+  logs.notice = noop
+}
+if (logLevel < LogLevels.verbose) {
+  logs.verbose = noop
 }
