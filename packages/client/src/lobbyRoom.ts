9e52ec375357187829f1c40b160d84d2d4b4039d
Darek Greenly <git@zielak.pl>
2020-08-15 15:26:29 +0200

    chore(client): restyle lobby room logs

--- packages/client/src/lobbyRoom.ts
+++ packages/client/src/lobbyRoom.ts
@@ -12,17 +12,17 @@ export class LobbyRoom {
   constructor(public room: colyseusRoom) {
     room.onMessage<RoomAvailable[]>("rooms", (rooms) => {
       this.onInit(rooms)
-      logs.verbose("LobbyINIT", rooms)
+      logs.verbose("Lobby", "init", rooms)
     })
 
     room.onMessage<[string, RoomAvailable]>("+", ([roomId, room]) => {
       this.onAdd(room)
-      logs.verbose("Lobby++", room.name, roomId)
+      logs.verbose("Lobby", "++", room.name, roomId)
     })
 
     room.onMessage<string>("-", (roomId) => {
       this.onRemove(roomId)
-      logs.verbose("Lobby--", roomId)
+      logs.verbose("Lobby", "--", roomId)
     })
 
     room.onLeave((code) => {
