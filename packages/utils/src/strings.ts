f35a1f045f203081dd4506c4b3e5604344285c8c
Darek Greenly <zielak@users.noreply.github.com>
2021-07-02 18:00:24 +0200

    chore(utils): add comments to string functions

--- packages/utils/src/strings.ts
+++ packages/utils/src/strings.ts
@@ -5,18 +5,27 @@ export const trim = (string = "", maxLength = 7): string => {
     : string.substr(0, maxLength - 1) + "…"
 }
 
+/**
+ * For now it's just 3 random letters
+ */
 export const randomName = (): string => {
   const randomLetter = (): string =>
     String.fromCharCode(Math.random() * (90 - 65) + 65)
   return randomLetter() + randomLetter() + randomLetter()
 }
 
+/**
+ * Convert string to "camelCase"
+ */
 export const camelCase = (str = ""): string =>
   str
     .replace(/\s(.)/g, ($1) => $1.toUpperCase())
     .replace(/\s/g, "")
     .replace(/^(.)/, ($1) => $1.toLowerCase())
 
+/**
+ * Convert string to "SentenceCase" (first letter capital)
+ */
 export const sentenceCase = (str = ""): string =>
   str
     .replace(/\s(.)/g, ($1) => $1.toUpperCase())
